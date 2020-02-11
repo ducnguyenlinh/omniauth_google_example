85126f845842bad5484598cdb3a0c431940820d3
ducnguyenlinh <duclinh0305@gmail.com>
2020-02-12 02:23:16 +0900

    modelを修正する

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(google_oauth2)
+
+  def self.from_omniauth(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(
+          email:      auth.info.email,
+          provider:   auth.provider,
+          uid:        auth.uid,
+          token:      auth.credentials.token,
+          expires_at: auth.credentials.expires_at,
+          password:   Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
 end
